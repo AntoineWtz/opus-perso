c66f8c83b5ddbac6716ca8fee898d89febe8e771
MartialGer <martial.geraud@gmail.com>
2023-06-13 11:36:51 +0200

    change publication 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,10 @@
-<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 <x-app-layout>
     <x-slot name="header">
-       
+        <h1>Profil de l'user</h1>
     </x-slot>
-    <h1>Profil de l'user</h1>
+    <x-slot name="slot">
+        <h2>contenue de l'user</h2>
+    </x-slot>
    
 </x-app-layout>
