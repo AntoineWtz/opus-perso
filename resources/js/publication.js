616eeab823f4e9b39213b9ae926f56278ad87976
MartialGer <martial.geraud@gmail.com>
2023-06-19 15:45:21 +0200

    next push

--- resources/js/publication.js
+++ resources/js/publication.js
@@ -1,13 +1,22 @@
 require('./bootstrap');
-
+require('jquery');
+require('select2');
+import Dropzone from 'dropzone';
+window.Dropzone = Dropzone;
+import $ from 'jquery';
+import 'select2';
+    
+    $('.js-example-basic-single').select2();
+    $('.js-example-basic-multiple').select2();
+    
 
 document.addEventListener('DOMContentLoaded', function() {
 
 
 
 //variable new genre musicaux
-var newGenreMusicauxbtn = document.getElementById('new-genre-musicaux-btn');
-var btnAnnulerGenreMusicaux = document.getElementById('annulerbtn');
+const newGenreMusicauxbtn = document.getElementById('new-genre-musicaux-btn');
+const btnAnnulerGenreMusicaux = document.getElementById('annulerbtn');
 //btn new genre musicaux
 newGenreMusicauxbtn.addEventListener('click' , function(){
 document.querySelector('div .modalgenremusical').classList.remove('hidden'); 
@@ -18,9 +27,30 @@ document.querySelector('div .modalgenremusical').classList.add('hidden');
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //variable new galerie
-var newGaleriebtn = document.getElementById('new-galerie1-btn');
-var btnAnnulerGalerie = document.getElementById('annuler-galerie-btn');
+const newGaleriebtn = document.getElementById('new-galerie1-btn');
+const btnAnnulerGalerie = document.getElementById('annuler-galerie1-btn');
 // btn new galerie
 newGaleriebtn.addEventListener('click' , function(){
 document.querySelector('div .modalGalerie').classList.remove('hidden');
@@ -29,10 +59,24 @@ document.querySelector('div .modalGalerie').classList.remove('hidden');
 btnAnnulerGalerie.addEventListener('click' , function(){
 document.querySelector('div .modalGalerie').classList.add('hidden');
 });
+//variable new galerie 2
+const newGaleriebtn2 = document.getElementById('new-galerie2-btn');
+const btnAnnulerGalerie2 = document.getElementById('annuler-galerie2-btn');
+// btn new galerie 2
+newGaleriebtn2.addEventListener('click' , function(){
+document.querySelector('div .modalGalerie2').classList.remove('hidden');
+    });
+// btn annuler gaelerie 2
+btnAnnulerGalerie2.addEventListener('click' , function(){
+    document.querySelector('div .modalGalerie2').classList.add('hidden');
+    });
+
+
+
 
 //variable new lieux
-var newLieuxbtn = document.getElementById('new-lieux-btn');
-var btnAnnulerLieux = document.getElementById('annuler-lieux-btn');
+const newLieuxbtn = document.getElementById('new-lieux-btn');
+const btnAnnulerLieux = document.getElementById('annuler-lieux-btn');
 //btn new lieux
 newLieuxbtn.addEventListener('click' , function(){
 document.querySelector('div .modalLieux').classList.remove('hidden');
@@ -42,6 +86,22 @@ btnAnnulerLieux.addEventListener('click' , function(){
 document.querySelector('div .modalLieux').classList.add('hidden');
 });
 
+
+
+const myDropzone = document.querySelector("#myDropzone")
+Dropzone.options.myDropzone = {
+    paramName: "photo",
+    acceptedFiles: ".jpeg,.jpg,.png",
+    dictDefaultMessage: "Déposez vos fichiers ici ou cliquez pour les sélectionner",
+    uploadMultiple: true,
+}
+const myDropzone2 = document.querySelector("#myDropzone2")
+Dropzone.options.myDropzone2 = {
+    paramName: "photo",
+    acceptedFiles: ".jpeg,.jpg,.png",
+    dictDefaultMessage: "Déposez vos fichiers ici ou cliquez pour les sélectionner",
+    uploadMultiple: true,
+}
 
 
 });
